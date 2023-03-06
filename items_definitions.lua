4fd51237ffffca9c0caba484716f406b6a4dbda4
ArchaicVirus <richardhall1029@gmail.com>
2023-03-06 18:54:41 -0500

    Add files via upload
    Separated items definitions to it's only file

---
+++ items_definitions.lua
@@ -0,0 +1,30 @@
+local items = {
+  [1] = {
+    name = 'red_circuit',
+    id = 1,
+    pixels = {
+      2,2,1,
+      3,4,2,
+      1,2,4,
+    }
+  },
+  [2] = {
+    name = 'green_circuit',
+    id = 2,
+    pixels = {
+      6,5,6,
+      3,4,5,
+      6,5,4,
+    }
+  },
+  [3] = {
+    name = 'iron_ore',
+    id = 3,
+    pixels = {
+      13,14,0,
+      14,15,13,
+      0,14,13,
+    }
+  }
+}
+return items
