d86b2e47ea0244a08998f828100949c4304e6e82
ArchaicVirus <richardhall1029@gmail.com>
2023-03-09 00:58:49 -0500

    Update power_pole.lua

--- classes/power_pole.lua
+++ classes/power_pole.lua
@@ -4,13 +4,25 @@ POWER_POLE_BTM_ID = 324
 POWER_CABLE_COLOR = 4
 POWER_CABLE_GREEN = 6
 POWER_CABLE_RED   = 2
+POWER_CURSOR_ID   = 320
+POWER_AREA_ID     = 336
+
+--for drawing sprites to show powered area
+POWER_AREA_MAP = {
+  [1] = {pos = {x = -16, y = -16}, rot = 0},
+  [2] = {pos = {x =  16, y = -16}, rot = 1},
+  [3] = {pos = {x =  16, y =  16}, rot = 2},
+  [4] = {pos = {x = -16, y =  16}, rot = 3}}
+
+  --for drawing the cables
 POWER_POLE_ANCHOR_POINTS = {
   ['power'] = {[1] = {x = 3, y = 1}, [2] = {x = 1, y = 4}},
   ['red']   = {[1] = {x = 0, y = 1}, [2] = {x = 1, y = 7}},
   ['green'] = {[1] = {x = 7, y = 1}, [2] = {x = 1, y = 0}}}
-POWER_POLE_CONNECT_MAP = {
 
-}
+  --tiles to check around self for other poles or powered objects
+POWER_POLE_CONNECT_MAP = {}
+
 local pole = {
   pos = {x = 0, y = 0},
   cables = {
@@ -19,18 +31,26 @@ local pole = {
     ['green'] = {}},
   range = 3,
   has_power = false,
+  is_hovered = false,
 }
 
-function pole.draw(self)
-  spr(POWER_POLE_TOP_ID, self.pos.x, self.pos.y - 16)
-  spr(POWER_POLE_MID_ID, self.pos.x, self.pos.y - 8 )
-  spr(POWER_POLE_BTM_ID, self.pos.x, self.pos.y     )
+function pole.draw(self, show_area)
+  spr(POWER_POLE_TOP_ID, self.pos.x, self.pos.y - 16, 0)
+  spr(POWER_POLE_MID_ID, self.pos.x, self.pos.y - 8 , 0)
+  spr(POWER_POLE_BTM_ID, self.pos.x, self.pos.y     , 0)
   for k, v in ipairs(self.cables) do
     if #v > 0 then
-      if k == 1 then col = 
+      if k == 1 then col = 1
       --if there are cables, draw them
       --poles can have multiple connections to other poles
+      end
     end
+  end
+  if show_area or self.is_hovered then
+    for k, v in ipairs(POWER_AREA_MAP) do
+      spr(POWER_AREA_ID, self.pos.x + v.pos.x, self.pos.y + v.pos.y, 0, 1, 0, v.rot)
+    end
+    spr(POWER_CURSOR_ID, self.pos.x, self.pos.y, 0)
   end
 end
 
