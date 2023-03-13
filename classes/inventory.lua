2f2db98462b2fee74a7de93f4b20288dbd3f768e
ArchaicVirus <richardhall1029@gmail.com>
2023-03-12 22:45:18 -0400

    Update inventory.lua
    Added slot hovering function, to get slot index hovered, and slot's world position. Slot index allows inventory slot lookup for item id, item count, etc.

--- classes/inventory.lua
+++ classes/inventory.lua
@@ -22,7 +22,6 @@ function inventory.draw(self)
       local x, y = (self.pos.x + 2) + ((col - 1) * (INVENTORY_SLOT_SIZE + 1)), (self.pos.y + 2) + ((row - 1) * (INVENTORY_SLOT_SIZE + 1))
       rect(x, y, INVENTORY_SLOT_SIZE, INVENTORY_SLOT_SIZE, INVENTORY_SLOT_COL)
       if row == INVENTORY_ROWS then
-        spr(288, x, y, 00, 1, 0, 0, 1, 1)
         if col > 9 then col = 0 end
         print(col, x + 2, y + 1, 0, true, 1, true)        
       end
@@ -53,10 +52,32 @@ function inventory.remove_item(self, slot)
 
 end
 
-function new_slot()
+function inventory.get_hovered_slot(self, x, y)
+  local inv_x, inv_y = self.pos.x, self.pos.y
+  local start_x = inv_x + 2
+  local start_y = inv_y + 2
+  
+  local rel_x = x - start_x + 1
+  local rel_y = y - start_y + 1
+  
+  local slot_x = math.floor(rel_x / 9)
+  local slot_y = math.floor(rel_y / 9)
+  
+  local slot_pos_x = start_x + slot_x * 9
+  local slot_pos_y = start_y + slot_y * 9
+  local slot_index = slot_y * 10 + slot_x + 1
+  if slot_x >= 0 and slot_x < 10 and slot_y >= 0 and slot_y < 10 then
+    return true, slot_pos_x, slot_pos_y, slot_index
+  else
+    return nil
+  end
+end
+
+function new_slot(index)
   local slot = {
     item_id = 0,
     count = 0,
+    index = index,
   }
   return slot
 end
@@ -64,9 +85,11 @@ end
 function create_inventory()
   local inv = {}
   setmetatable(inv, {__index = inventory})
+  local i = 1
   for row = 1, INVENTORY_ROWS do
     for col = 1, INVENTORY_COLS do
-      table.insert(inv.slots, new_slot())
+      inv.slots[i] = new_slot(i)
+      i = i + 1
     end
   end
   return inv
