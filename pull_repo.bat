9efb86b07823c63a313f735bc54659a83de0455f
ArchaicVirus <richardhall1029@gmail.com>
2023-04-26 04:06:41 -0400

    Update .gitignore to exclude ignored files

--- pull_repo.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Fetching changes from the remote repository...
-git fetch origin remote
-echo.
-echo Resetting to the latest remote state...
-git reset --hard origin/remote
-echo.
-echo Done.
-pause
--- start.bat
+++
@@ -1,3 +0,0 @@
-rem @echo off 
-start /b tic80.exe --fs . --cmd="load craptorio_main.lua" 
-exit 
