d810b8d32a5161ab4f4c5b0cc16e9b249d50285d
ArchaicVirus <richardhall1029@gmail.com>
2023-05-01 12:49:37 -0400

    more furnace ui updates

--- classes/furnace.lua
+++ classes/furnace.lua
@@ -20,13 +20,13 @@ local Furnace = {
   drawn = false,
   fuel_slots = FURNACE_BUFFER_FUEL,
   output_slots = FURNACE_BUFFER_SIZE,
-  output_buffer = {15},
-  input_buffer = {3},
-  fuel_buffer = {},
+  output_buffer = {},
+  input_buffer = {},
+  fuel_buffer = {6,6,6,6,6,6,6,6,6},
   dummy_keys = {},
   fuel_time = 0,
-  smelt_timer = FURNACE_SMELT_TIME,
-  ore_type = nil,
+  smelt_timer = 0,
+  ore_type = false,
   is_smelting = false,
   item_id = 14,
 }
@@ -71,7 +71,7 @@ end
 
 function Furnace.open(self)
   return {
-    x = cursor.x + 5,
+    x = 240 - 103,
     y = 3,
     width = 100,
     height = 100,
@@ -100,16 +100,46 @@ function Furnace.open(self)
       local x, y, w, h = self.x, self.y, self.width, self.height
       --background window and border
       rectb(x, y, w, h, 13)
-      rect(x + 1, y + 1, w - 2, h - 2, 0)
+      rect(x + 1, y + 1, w - 2, h - 2, 15)
 
       --input slots icon, and item count------------------------------------------
-      -- local text = #input .. '/' .. FURNACE_BUFFER_INPUT
-      -- local text_width = print(text, 0, -10, 0, true, 1, true)
-      -- local sprite_id = 311
-      -- print('Input - ', x + 3, y + 3, 11, true, 1, true)
-      -- print(text, x + w - 2 - text_width, y + 3, 4, true, 1, true)
-      -- if #input > 0 then sprite_id = ITEMS[input[1]].sprite_id end
-      -- sspr(sprite_id, x + w - 12 - text_width, y + 2, 4)
+
+      --input slot
+      rectb(x + 9, y + 58, 10, 10, 14)
+      rect(x + 10, y + 59, 8, 8, 0)
+      print(#input .. '/' .. FURNACE_BUFFER_INPUT, x + 9, y + 70, 4, true, 1, true)
+      if #input > 0 then sspr(ITEMS[input[1]].sprite_id, x + 10, y + 59, 4) end
+
+      --smelting progress bar
+      rectb(x + 20, y + 61, 60, 5, 14)
+      rect(x + 21, y + 62, 58, 3, 0)
+      if ent.smelt_timer > 0 then
+        rect(x + 21, y + 62, 58 - remap(ent.smelt_timer, 0, FURNACE_SMELT_TIME, 0, 58), 3, 6)
+      end
+      --output slot
+      rectb(x + w - 19, y + 58, 10, 10, 14)
+      rect(x + w - 18, y + 59, 8, 8, 0)
+      local text_width = print(#output .. '/' .. FURNACE_BUFFER_OUTPUT, 0, -10, 0, true, 1, true)
+      print(#output .. '/' .. FURNACE_BUFFER_OUTPUT, x + w - 8 - text_width, y + 70, 4, true, 1, true)
+      if #output > 0 then sspr(ITEMS[output[1]].sprite_id, x + w - 18, y + 59, 0) end
+
+      --divider
+      line(x + 9, y + 77, x + w - 10, y + 77, 13)
+
+      --fuel slot
+      rectb(x + 9, y + 80, 10, 10, 14)
+      if #fuel > 0 then
+        sspr(ITEMS[fuel[1]].sprite_id, x + 10, y + 81, -1)
+      else
+        sspr(310, x + 10, y + 81, -1)
+      end
+      --fuel progress bar
+      rectb(x + 20, y + 84, 60, 5, 14)
+      rect(x + 21, y + 85, 58, 3, 0)
+      if ent.fuel_time > 0 then
+        rect(x + 21, y + 85, remap(ent.fuel_time, 0, ITEMS[6].smelting_time, 0, 58), 3, 2)
+      end
+      print(#fuel .. '/' .. FURNACE_BUFFER_FUEL, x + 9, y + 91, 4, true, 1, true)
 
       --output slots icon and item count---------------------------------------------
       -- local text = #output .. '/' .. FURNACE_BUFFER_OUTPUT
@@ -128,7 +158,8 @@ function Furnace.open(self)
       -- print(text, x + w - 2 - text_width, y + 24, 4, true, 1, true)
       -- if #fuel > 0 then sprite_id = ITEMS[fuel[1]].sprite_id end
       -- sspr(sprite_id, x + w - 12 - text_width, y + 24, 4)
-      --large stone furnace graphic------------------------------------------------
+
+      --large stone furnace graphic-and terrain background-------------------------
       local sprite_id = FURNACE_SPRITE_INACTIVE_ID
       local fx, fy = x + (w / 2) - 8, y + 22 --furnace icon screen pos
       if ent.is_smelting then sprite_id = FURNACE_SPRITE_ACTIVE_ID + (FURNACE_ANIM_TICK * 2) end
@@ -149,13 +180,21 @@ end
 
 function Furnace.update(self)
   --update fuel ticks
-  if self.fuel_time <= 0 then
-    if #self.fuel_buffer > 0 then
-      local item_id = table.remove(self.fuel_buffer)
-      self.fuel_time = ITEMS[item_id].smelting_time
-    end
+  if self.fuel_time > 0 then
+    self.fuel_time = self.fuel_time - FURNACE_TICKRATE
   end
 
+  if self.fuel_time <= 0 then
+    --if we run out of fuel_time, check fuel_buffer to re-fuel self
+    if self.is_smelting then
+      if #self.fuel_buffer > 0 then
+        self.fuel_time = ITEMS[table.remove(self.fuel_buffer)].smelting_time
+      else
+        --otherwise we have ran out of fuel completely, so shut-down
+        self.is_smelting = false
+      end
+    end
+  end
 
   if self.is_smelting then
     --update smelting countdown timer
@@ -164,20 +203,23 @@ function Furnace.update(self)
       --smelting operation completed
       --pop last item from output_buffer to input_buffer
       self.is_smelting = false
-      if #self.input_buffer > 0 then
-        local ore = table.remove(self.input_buffer)
-        local smelted_ore = ITEMS[ore].smelted_id
-        table.insert(self.output_buffer, smelted_ore)
-        return
-      end
+      local ore = table.remove(self.input_buffer)
+      local smelted_ore = ITEMS[ore].smelted_id
+      table.insert(self.output_buffer, smelted_ore)
+      return
     end
-  else
-    --check for incoming ore
-    if not self.is_smelting and #self.input_buffer > 0 and self.fuel_time > 0 and #self.output_buffer < FURNACE_BUFFER_OUTPUT then
-      self.is_smelting = true
-      self.smelt_timer = FURNACE_SMELT_TIME
-    end
+  end
 
+
+    --check for incoming ore
+  if not self.is_smelting and #self.input_buffer > 0 and #self.output_buffer < FURNACE_BUFFER_OUTPUT then
+    self.is_smelting = true
+    self.smelt_timer = FURNACE_SMELT_TIME
+  end
+  trace('#FURNACE INPUT BUFFER: ' .. #self.input_buffer)
+  trace('#FURNACE OUTPUT BUFFER: ' .. #self.output_buffer)
+  if #self.input_buffer == 0 and #self.output_buffer == 0 then
+    self.ore_type = false
   end
 end
 
@@ -191,8 +233,9 @@ function Furnace.draw(self)
 end
 
 function Furnace.deposit(self, item_id, keep)
+  keep = keep or false
   local item = ITEMS[item_id]
-  trace(item.type)
+  --trace(item.type)
   if item.type == 'fuel' then
     if #self.fuel_buffer < 5 then
       if not keep then table.insert(self.fuel_buffer, item_id) end
@@ -200,18 +243,28 @@ function Furnace.deposit(self, item_id, keep)
     end
   end
   if item.type ~= 'ore' then return end
-  if not self.ore_type then
+  trace('try deposit ore')
+  trace('FURNACE ORE TYPE: ' .. tostring(self.ore_type))
+  trace('ITEM ORE TYPE: ' .. item.name)
+  trace('#FURNACE INPUT BUFFER: ' .. #self.input_buffer)
+  trace('#FURNACE OUTPUT BUFFER: ' .. #self.output_buffer)
+  -- if not self.ore_type then
+  --   if not keep then
+  --     self.ore_type = item.name
+  --     table.insert(self.input_buffer, item_id)
+  --   end
+  --   return true
+  if #self.input_buffer < FURNACE_BUFFER_INPUT
+  and self.ore_type == item.name or not self.ore_type then
+
     if not keep then
       self.ore_type = item.name
-      table.insert(self.input_buffer, item_id)
-    end
-    return true
-  elseif #self.input_buffer < FURNACE_BUFFER_INPUT and self.ore_type == item.name then
-    if not keep then
       table.insert(self.input_buffer, item_id) 
     end
     return true
+
   end
+
   return false
 end
 
